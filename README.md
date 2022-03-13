f46670f99cf24bfa1691637ffa1396ebff35de7a
Justin Garner <j.garner828@gmail.com>
2022-03-13 17:31:58 -0400

    completed User model

---
+++ README.md
@@ -0,0 +1,18 @@
+# Social Media Backend
+  
+  [![MITLicense](https://img.shields.io/badge/License-MITLicense-<COLOR>.svg)](https://shields.io/)
+  
+  ## https://github.com/jgarner828/Social-Network
+  
+  # Project Description
+  
+  **Social Network API built using Mongoose and Express and Mongoose. API routes using REST architecture.**
+
+  # Usage
+
+  **Use command `npm start` to run server. Access routes via HTTP requests.**
+  
+  # License
+  
+  ### [MIT License Link](https://opensource.org/licenses/MIT)
+  
--- models/User.js
+++ models/User.js
@@ -1,33 +1,48 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
+const Thought = require('./Thought');
+
+
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
+
 
 // Schema to create User model
 const userSchema = new Schema(
   {
-    first: {
+    username: {
       type: String,
+      unique: true,
       required: true,
-      max_length: 50,
+      max_length: 20,
     },
-    last: {
+    email: {
       type: String,
-      required: true,
       max_length: 50,
-    },
-    github: {
-      type: String,
+      unique: true,
       required: true,
-      max_length: 50,
+      validate: [validateEmail, 'Please fill a valid email address'],
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
-    reactions: [reactionSchema],
+    thoughts: [Thought],
+    friends: [User],
   },
+
   {
     toJSON: {
+      virtuals: true,
       getters: true,
     },
   }
 );
 
-const User = model('user', userSchema);
+// virtual retrieves length of friends array.
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+})
+
+const User = model('User', userSchema);
 
 module.exports = User;
